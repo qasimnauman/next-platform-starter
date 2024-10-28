7e83e010eebf3888319f2257991b8db5d375e83e
Muhammad Qasim Nauman <123264838+qasimnauman@users.noreply.github.com>
2024-10-28 07:08:18 -0700

    Update deploy_nextjs.sh

--- .github/scripts/deploy_nextjs.sh
+++ .github/scripts/deploy_nextjs.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-# Directory where the app will be stored on EC2
-REMOTE_APP_DIR="~/app"
+# Accessing secrets from GitHub environment variables
+EC2_USERNAME="${{ secrets.EC2_USERNAME }}"
+EC2_HOST="${{ secrets.EC2_HOST }}"
 
-# Initial system update and install dependencies
-echo "Updating the system and installing dependencies..."
-sudo apt-get update -y && sudo apt-get upgrade -y
-sudo apt-get install nodejs -y
-sudo apt-get install npm -y
-sudo npm install pm2 -g
+# Absolute path where the app will be stored on EC2
+REMOTE_APP_DIR="/home/ubuntu/app"
 
 # Sync code to the EC2 instance
 echo "Syncing code to EC2 instance..."
@@ -16,7 +13,18 @@ rsync -avz --delete -e "ssh -i ~/.ssh/id_rsa" . $EC2_USERNAME@$EC2_HOST:$REMOTE_
 
 # SSH into the instance to handle the directory and deploy
 ssh -i ~/.ssh/id_rsa $EC2_USERNAME@$EC2_HOST << 'EOF'
-  REMOTE_APP_DIR="/home/ubuntu/app"  # Use the absolute path here
+  # Initial system update and install dependencies
+  echo "Updating the system and installing dependencies..."
+  sudo apt-get update -y
+  sudo apt-get install -y nodejs npm
+
+  # Check if PM2 is installed, if not, install it
+  if ! command -v pm2 &> /dev/null; then
+    echo "PM2 is not installed, installing PM2 globally..."
+    sudo npm install pm2 -g
+  else
+    echo "PM2 is already installed."
+  fi
 
   # Ensure the application directory exists or create it if missing
   if [ -d "$REMOTE_APP_DIR" ]; then
