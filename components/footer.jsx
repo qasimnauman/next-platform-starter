785a713fefa98e999ca0a6c7c10fb0dc9d12263d
Muhammad Qasim Nauman <123264838+qasimnauman@users.noreply.github.com>
2024-11-24 07:39:00 -0800

    Update footer.jsx

--- components/footer.jsx
+++ components/footer.jsx
@@ -5,7 +5,7 @@ export function Footer() {
         <footer className="pt-16 pb-12 sm:pt-24 sm:pb-16">
             <p className="text-sm">
                 <Link href="https://docs.netlify.com/frameworks/next-js/overview/" className="underline transition decoration-dashed text-primary underline-offset-8 hover:opacity-80">
-                    Next.js on Netlify
+                    Next.js on Netlify from Dev branch
                 </Link>
             </p>
         </footer>
